128fbec718c0c4cdb115b5ea2d0ad5e767b32d9a
Salvelop07 <salvador.h.1004@gmail.com>
2024-10-03 13:45:49 +0100

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Add your Next.js configuration options here
+  reactStrictMode: true,
+  // Example: Enable image optimization
+  images: {
+    domains: ["example.com"],
+  },
+};
 
 export default nextConfig;
