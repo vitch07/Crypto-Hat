e6c822e7e5613e3dfff752b2ef7e1e5fa709940a
Salvelop07 <salvador.h.1004@gmail.com>
2024-10-20 11:17:56 +0100

    created navbar

---
+++ component/Button.jsx
@@ -0,0 +1,7 @@
+function Button() {
+  <button type="button" className="nft-gradient">
+    Button
+  </button>;
+}
+
+export default Button;
--- component/index.js
+++ component/index.js
@@ -1,4 +1,5 @@
 import Navbar from "./Navbar.jsx";
 import Footer from "./Footer.jsx";
+import Button from "./Button.jsx";
 
-export { Navbar, Footer };
+export { Navbar, Footer, Button };
