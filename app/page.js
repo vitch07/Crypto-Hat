b9cb721bd18499ec2879eca583dc90bdc45c69ae
Salvelop07 <salvador.h.1004@gmail.com>
2024-10-24 00:44:54 +0100

    finished banner

--- app/page.js
+++ app/page.js
@@ -1,7 +1,15 @@
+import Banner from "@/component/Banner";
+
 export default function Home() {
   return (
-    <div>
-      <p className="text-center font-bold text-4xl">Welcome</p>
+    <div className="flex justify-center sm:px-4 p-12">
+      <div className="w-full minmd:w-4/5">
+        <Banner
+          name="Discover, collect and sell extraordinary NFTs"
+          childStyles="md:text-4xl sm:text-2xl xs:text-xl text-left text-white"
+          parentStyles="justify-start mb-6 h-72 sm:h-60 p-12 xs:p-4 xs:h-44 rounded-3xl"
+        />{" "}
+      </div>
     </div>
   );
 }
