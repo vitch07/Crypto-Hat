8c255db26ff68711262ba8dc2a7201fe28863a22
Salvelop07 <salvador.h.1004@gmail.com>
2024-10-24 17:51:51 +0100

    finished topseller component

--- component/Topseller.jsx
+++ component/Topseller.jsx
@@ -1,12 +1,46 @@
 "use client";
 import { useState, useEffect, useRef } from "react";
-import Image from "next/image";
 import CreatorCard from "./CreatorCard";
 import images from "../assets";
 import makeid from "@/utils/makeid";
+import { IoIosArrowDropleft, IoIosArrowDropright } from "react-icons/io";
+
 function Topseller() {
+  const [hideButtons, setHideButtons] = useState(false);
   const parentRef = useRef(null);
   const scrollRef = useRef(null);
+
+  const handleScroll = (direction) => {
+    const { current } = scrollRef;
+
+    const scrollAmount = window.innerWidth > 1000 ? 270 : 210;
+
+    if (direction == "left") {
+      current.scrollLeft -= scrollAmount;
+    } else {
+      current.scrollLeft += scrollAmount;
+    }
+  };
+
+  const isScrollable = () => {
+    const { current } = scrollRef;
+    const { current: parent } = parentRef;
+
+    if (current?.scrollWidth >= parent?.offsetWidth) {
+      setHideButtons(false);
+    } else {
+      setHideButtons(true);
+    }
+  };
+
+  useEffect(() => {
+    isScrollable();
+    window.addEventListener("resize", isScrollable);
+
+    return () => {
+      window.removeEventListener("resize", isScrollable);
+    };
+  });
 
   return (
     <div>
@@ -18,7 +52,7 @@ function Topseller() {
           className="flex flex-row w-max overflow-x-scroll no-scrollbar select-none"
           ref={scrollRef}
         >
-          {[6, 7, 8, 9, 10].map((i) => (
+          {[1, 2, 3, 4, 5, 6].map((i) => (
             <CreatorCard
               key={`creator-${i}`}
               rank={i}
@@ -27,17 +61,33 @@ function Topseller() {
               creatorEths={10 - i * 0.5}
             />
           ))}
-          <>
-            <div>
-              <Image
-                src={images.left}
-                layout="fill"
-                objectFit="contain"
-                alt="left-arrow"
-                className="bg-nft-dark-1"
-              />
-            </div>
-          </>
+
+          {!hideButtons && (
+            <>
+              <div
+                onClick={() => handleScroll("left")}
+                className="absolute w-8 h-8 minlg:w-12 minlg:h-12 top-45 cursor-pointer left-0"
+              >
+                <IoIosArrowDropleft
+                  layout="fill"
+                  objectfit="contain"
+                  alt="left-arrow"
+                  className="w-7 h-7"
+                />
+              </div>
+              <div
+                onClick={() => handleScroll("right")}
+                className="absolute w-8 h-8 minlg:w-12 minlg:h-12 top-45 cursor-pointer right-0"
+              >
+                <IoIosArrowDropright
+                  layout="fill"
+                  objectfit="contain"
+                  alt="right-arrow"
+                  className="w-7 h-7"
+                />
+              </div>
+            </>
+          )}
         </div>
       </div>
     </div>
